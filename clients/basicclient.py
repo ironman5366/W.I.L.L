4598cd0d599483d6f673930bdf81d5733209caa0
Will Beddow <will.beddow@gmail.com>
2015-10-15 10:35:37 -0400

    Fixed the basic client

--- clients/basicclient.py
+++ clients/basicclient.py
@@ -4,13 +4,13 @@ import urllib
 import sys
 def main(command):
 	command=urllib.urlencode({"command":command})
-	answer=urllib.urlopen("http://127.0.0.1:5000?context=command&%s"%command)
+	answer=urllib.urlopen("http://127.0.0.1:5000?context=command&%s"%command).read()
 	easygui.msgbox(answer)
 while True:
 	command=easygui.enterbox(title="W.I.L.L.", msg="Please enter a command")
 	if command=="exit":
 		sys.exit()
 	elif command==None:
-		easygui.msgbox("Please enter a command")
+		sys.exit()
 	else:
 		main(command)
