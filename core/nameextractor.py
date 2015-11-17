8c2d2e1844347bbe1f25c68ead5a8827a31af5f4
Will Beddow <will.beddow@gmail.com>
2015-11-17 11:43:04 -0500

    Removed uncecessary and inefficient POS
    POS tagging wasn't necessary anymore as .will files and a list of them dictate what words W.I.L.L. knows. This along with the fact that POS tagging took the majority of the processing time meant it was time to remove it.

--- core/nameextractor.py
+++ core/nameextractor.py
@@ -1,20 +1,5 @@
 contacts = open('core/content/contacts.txt').read().split('\n')
-from textblob import TextBlob
 import re
-
-
-def pos(text):
-    blob = TextBlob(text)
-    lst = blob.tags
-    grammar_tags = []
-    for i in range(0, len(lst)):
-        y = re.findall(r'([N][N][P]*[S]*)+', lst[i][1])
-        if len(y) != 0:
-            grammar_tags.append(y[0])
-        else:
-            grammar_tags.append("None")
-    return grammar_tags
-
 
 def name_check(word):
     for item in contacts:
@@ -34,10 +19,8 @@ def name_check(word):
 
 def main(text):
     text_words = text.split(" ")
-    grammar_tags = pos(text)
     names = []
-    for i in range(0, len(grammar_tags)):
-        if grammar_tags[i] != "None":
+    for i in range(0, len(text_words)):
             try:
                 word = text_words[i].lower()
                 check_flag = name_check(word)
