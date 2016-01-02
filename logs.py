f813a448bda91470de2650005e264cdeb565b621
ironman5366 <will.beddow@gmail.comgit config --global user.email will.beddow@gmail.com>
2016-01-02 17:51:32 -0500

    Logging code

---
+++ logs.py
@@ -0,0 +1,58 @@
+from termcolor import colored
+import datetime
+class logs():
+	def keepcheck(self):
+		f=open("settings.conf")
+		s=f.read()
+		slines=s.split("\n")
+		f.close()
+		for line in slines:
+			if 'keep=' in line:
+				mode=line.split('=')[1]
+				if mode=='true':
+					return True
+				else:
+					return False
+			else:
+				pass
+	def debug(self):
+		f=open("settings.conf")
+		s=f.read()
+		slines=s.split("\n")
+		f.close()
+		for line in slines:
+			if 'debug=' in line:
+				mode=line.split('=')[1]
+				if mode=='true':
+					return True
+				else:
+					return False
+			else:
+				pass
+	def write(self, item, logtype):
+		logtype=logtype.lower()
+		if logtype=="error":
+			color='red'
+		elif logtype=="success":
+			color='green'
+		elif logtype=='trying':
+			color='yellow'
+		elif logtype=='working':
+			color='blue'
+		else:
+			color='white'
+		logtime=('[{0}]'.format(str(datetime.datetime.now()).split(" ")[1].split('.')[0]))
+		logitem=colored(item,color)
+		finallog=("{0}: {1}".format(logtime,item))
+		debuglog=("{0}: {1}".format(logtime,logitem))
+		debug=self.debug()
+		if self.keepcheck():
+			try:
+				logfile=open("WILL.log",'a')
+			except IOError:
+				logfile=open("WILL.log",'w')
+		else:
+			logfile=open("WILL.log",'w')
+		logfile.write('{0}\n'.format(finallog))
+		if debug:
+			print debuglog
