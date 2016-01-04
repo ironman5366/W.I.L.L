86c68b4497121370d7ac59356996ab944cc4e3d3
ironman5366 <will.beddow@gmail.comgit config --global user.email will.beddow@gmail.com>
2016-01-04 14:23:58 -0500

    Unfinished & Untested additions

--- intent.py
+++ intent.py
@@ -5,16 +5,21 @@ logs=log()
 
 def parse(command, plugins):
 	words=command.split(' ')
-	for word in words:
+	with words[0] as word:
 		verb=False
 		logs.write('Analyzing word {0}'.format(word), 'working')
 		logs.write('Checking to see if the word is a plugin', 'trying')
-		for plugin in glob.glob("plugins/*.will"):
-			plugname=plugin.split('.will')[0].split('plugins/')[1]
-			logs.write("Plugin name is {0}".format(plugname),'working')
+		for plugin in plugins:
+			logs.write("Plugin name is {0}".format(plugin[0]),'working')
+			syns=plugin[7][1]
 			if command.lower()==plugname.lower():
 				logs.write("The command and plugin name match",'success')
-				return plugname
+				return plugins[plugname]
 			else:
+				for syn in syns:
+					logs.write("Checking synonym {0}".format(syn), 'working')
+					if command.lower()==syn:
+						logs.write()
 				logs.write("The command does not match the plugin name",'trying')
+	#At this point, the first word didn't match a plugin or it's synonyms
 				
