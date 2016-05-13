e96b1eb2d7e84d1402bde1588bbc951b60f7694f
Brent Taylor <btaylor@fuzzylogicstudios.com>
2016-05-12 18:14:53 -0700

    Add will.plugin.pyplugins.PythonLoader
    Add unit tests for PythonLoader.is_plugin success states
    Add unit tests for PythonLoader.import_name

--- will/plugins/pyplugins.py
+++ will/plugins/pyplugins.py
@@ -14,6 +14,28 @@ EVT_ANY_INPUT = "will_evt_any_input"
 _event_handlers = {}
 
 
+def plugin_loader(path):
+    python_loader = PythonLoader(path)
+
+    try:
+        python_loader.load()
+    except IOError:
+        return
+
+
+def unload_all():
+    dispatcher.send(EVT_EXIT)
+
+    # note, a copy of the _event_handlers[event] list is made here with the [:]
+    # syntax as we are going to be removing event handlers from the list and
+    # we can't do this while iterating over the same list.
+
+    for event, handlers in _event_handlers.iteritems():
+        for handler in handlers[:]:
+            dispatcher.disconnect(handler, signal=event)
+            _event_handlers[event].remove(handler)
+
+
 def event(events):
     def subscribe(evt, func):
         dispatcher.connect(func, signal=evt)
@@ -30,6 +52,38 @@ def event(events):
         return func
 
     return decorator
+
+
+class PythonLoader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def load(self):
+        pass
+
+    def is_plugin(self, fs_tools=os.path):
+        if fs_tools.exists(self.file_path):
+            if fs_tools.isfile(self.file_path):
+                return True
+            if fs_tools.isdir(self.file_path):
+                init_file = os.path.join(self.file_path, "__init__.py")
+                if fs_tools.exists(init_file) and fs_tools.isfile(init_file):
+                    return True
+        return False
+
+    def import_name(self):
+        if self.file_path.endswith('.py'):
+            return os.path.basename(self.file_path).split('.')[0]
+        else:
+            return os.path.basename(self.file_path)
+
+    def update_path(self):
+        pass
+
+    def _lib_path(self):
+        return os.path.normpath(
+            os.sep.join(os.path.normpath(self.file_path).split(os.sep)[:-1])
+        )
 
 
 def get_import_name(path, fs_tools=os.path):
@@ -61,24 +115,11 @@ def load_plugin_meta_data(path):
         )
 
 
-def plugin_loader(path):
+def plugin_loader_old(path):
     meta_data = load_plugin_meta_data(path)
     if meta_data.is_plugin:
         log.info("Loading plugin: {0}".format(path))
         if meta_data.lib_path not in sys.path:
             sys.path.append(meta_data.lib_path)
         importlib.import_module(meta_data.import_name)
-
-
-def unload_all():
-    dispatcher.send(EVT_EXIT)
-
-    # note, a copy of the _event_handlers[event] list is made here with the [:]
-    # syntax as we are going to be removing event handlers from the list and
-    # we can't do this while iterating over the same list.
-
-    for event, handlers in _event_handlers.iteritems():
-        for handler in handlers[:]:
-            dispatcher.disconnect(handler, signal=event)
-            _event_handlers[event].remove(handler)
 
---
+++ will/plugins/test_pythonLoader.py
@@ -0,0 +1,37 @@
+import os
+from will.unittests import TestCase
+from expects import *
+from mock import MagicMock, patch
+from will.plugins.pyplugins import PythonLoader
+
+
+class PythonLoader_ImportName(TestCase):
+    def test_ShouldReturnModuleNameSutableForImport(self):
+        plugin_file = PythonLoader("plugin/my_plugin.py")
+        plugin_module = PythonLoader("plugin/my_plugin") # Module dir
+
+        expect(plugin_file.import_name()).to(equal("my_plugin"))
+        expect(plugin_module.import_name()).to(equal("my_plugin"))
+
+
+class PythonLoader_IsPlugin(TestCase):
+    @patch('os.path.exists')
+    @patch('os.path.isfile')
+    def test_ShouldReturnTrueIfPluginIsFileAndExists(self, isfile, exists):
+        plugin_file = PythonLoader("plugin/my_plugin.py")
+        isfile.return_value = True
+        exists.return_value = True
+
+        expect(plugin_file.is_plugin(fs_tools=os.path)).to(be_true)
+
+    @patch('os.path.isfile')
+    @patch('os.path.exists')
+    @patch('os.path.isdir')
+    def test_ShouldReturnTrueifPluginIsDirAndExists(self, isdir, exists,
+                                                    isfile):
+        isfile.side_effect = [False, True]
+        exists.return_value = True
+        isdir.return_value = True
+        plugin_module = PythonLoader("plugin/my_plugin")
+
+        expect(plugin_module.is_plugin(fs_tools=os.path)).to(be_true)
