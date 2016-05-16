b0fe11b8a4e66801961141e9293666a7c0a2af0e
Brent Taylor <btaylor@fuzzylogicstudios.com>
2016-05-15 23:46:15 -0700

    Add unit tests for PythonLoader.is_plugin

--- will/plugins/test_pythonLoader.py
+++ will/plugins/test_pythonLoader.py
@@ -1,14 +1,14 @@
 import os
 from will.unittests import TestCase
-from expects import *
-from mock import MagicMock, patch
+from expects import *  # noqa
+from mock import patch
 from will.plugins.pyplugins import PythonLoader
 
 
 class PythonLoader_ImportName(TestCase):
     def test_ShouldReturnModuleNameSutableForImport(self):
         plugin_file = PythonLoader("plugin/my_plugin.py")
-        plugin_module = PythonLoader("plugin/my_plugin") # Module dir
+        plugin_module = PythonLoader("plugin/my_plugin")  # Module dir
 
         expect(plugin_file.import_name()).to(equal("my_plugin"))
         expect(plugin_module.import_name()).to(equal("my_plugin"))
@@ -43,3 +43,25 @@ class PythonLoader_IsPlugin(TestCase):
 
         expect(plugin_file.is_plugin(fs_tools=os.path)).to(be_false)
 
+    @patch('os.path.exists')
+    @patch('os.path.isfile')
+    @patch('os.path.isdir')
+    def test_ShouldReturnFalseIfInitFileDoesNotExistInDir(self, isdir, isfile,
+                                                          exists):
+        exists.side_effect = [True, False]
+        isfile.side_effect = [False, True]
+        isdir.return_value = True
+        plugin_file = PythonLoader("plugins/my_plugin")
+
+        expect(plugin_file.is_plugin(fs_tools=os.path)).to(be_false)
+
+    @patch('os.path.exists')
+    @patch('os.path.isfile')
+    @patch('os.path.isdir')
+    def test_ShouldReturnFalseIfInitIsNotAFile(self, isdir, isfile, exists):
+        exists.return_value = True
+        isfile.return_value = False
+        isdir.return_value = True
+        plugin_file = PythonLoader("plugins/my_plugin")
+
+        expect(plugin_file.is_plugin(fs_tools=os.path)).to(be_false)
