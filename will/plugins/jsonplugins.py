9e919740e64097f7f8599c6d4ddf9a1a1b008704
Brent Taylor <btaylor@fuzzylogicstudios.com>
2016-05-25 22:32:13 -0700

    Add support for "init" and "shutdown" events to json plugins

--- will/plugins/jsonplugins.py
+++ will/plugins/jsonplugins.py
@@ -19,12 +19,13 @@ def plugin_loader(file_path):
 
 
 class JsonLoader:
+
     def __init__(self, file_path):
         self.file_path = file_path
 
     def is_json_file(self, fs_tools=os.path):
         if self.file_path.endswith('.json'):
-            if fs_tools.exists(self.file_path) and fs_tools.isfile(self.file_path):
+            if fs_tools.exists(self.file_path) and fs_tools.isfile(self.file_path):  # noqa
                 return True
         return False
 
@@ -36,14 +37,74 @@ class JsonLoader:
         raise IOError
 
 
+class PluginBuilder:
+
+    def __init__(self, plugin_data):
+        self.plugin_data = plugin_data
+
+    @staticmethod
+    def thread_shell_call(command):
+        thread = threading.Thread(target=lambda: subprocess.call(
+            command, shell=True
+        ))
+        thread.daemon = True
+        thread.start()
+
+    def build(self):
+        self.build_init_callback()
+        self.build_subscribed_callback()
+        self.build_shutdown_callback()
+
+    def build_init_callback(self):
+        if "init" in self.plugin_data:
+            @API.init
+            def plugin():
+                PluginBuilder.thread_shell_call(
+                    self.plugin_data["init"]
+                )
+
+    def build_subscribed_callback(self):
+        if "key_words" in self.plugin_data:
+            # make sure everything in the list is a string
+            key_words = map(lambda x: str(x),
+                self.plugin_data["key_words"]  # noqa
+            )  # noqa
+
+            @API.subscribe_to(key_words)
+            def plugin(leader, full_text):
+                PluginBuilder.thread_shell_call(
+                    self.plugin_data["command"].format(
+                        full_text[len(leader) + 1:]
+                    )
+                )
+        else:
+            @API.subscribe_to_any
+            def plugin(full_text):
+                def func():
+                    PluginBuilder.thread_shell_call(
+                        self.plugin_data["command"].format(full_text)
+                    )
+
+    def build_shutdown_callback(self):
+        if "shutdown" in self.plugin_data:
+            @API.shutdown
+            def plugin():
+                PluginBuilder.thread_shell_call(
+                    self.plugin_data["shutdown"]
+                )
+
+
 class JsonData:
+
     def __init__(self, json_data):
         self.plugin_data = json_data
 
     def is_valid(self):
         validation_schema = Schema({
-           "key_words": list,
-           Required("command"): unicode
+            "key_words": list,
+            "init": unicode,
+            "shutdown": unicode,
+            Required("command"): unicode
         })
         try:
             validation_schema(self.plugin_data)
@@ -54,37 +115,4 @@ class JsonData:
 
     def build_plugin(self):
         if self.is_valid():
-            if "key_words" in self.plugin_data:
-
-                # make sure everything in the list is a string
-                key_words = map(lambda x: str(x), self.plugin_data["key_words"])
-
-                @API.subscribe_to(key_words)
-                def plugin(leader, full_text):
-
-                    def func():
-                        subprocess.call(
-                            self.plugin_data["command"].format(
-                                full_text[len(leader)+1:]
-                            ),
-                            shell=True
-                        )
-
-                    thread = threading.Thread(target=func)
-                    thread.daemon = True
-                    thread.start()
-
-            else:
-
-                @API.subscribe_to_any
-                def plugin(full_text):
-                    def func():
-                        subprocess.call(
-                            self.plugin_data["command"].format(full_text),
-                            shell=True
-                        )
-
-                    thread = threading.Thread(target=func)
-                    thread.daemon = True
-                    thread.start()
-
+            PluginBuilder(self.plugin_data).build()
