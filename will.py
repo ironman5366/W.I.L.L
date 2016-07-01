f8ea06ee3de0133be9f8e54d41ebd9f5eb5ecfd5
Will Beddow <will.beddow@gmail.com>
2016-07-01 19:15:33 -0400

    Final Preparations for merge

--- will.py
+++ will.py
@@ -1,19 +1,3 @@
 import will
-import easygui
-import sys
 
 will.run()
-
-#Basic client debugging code
-def command():
-    command_str = easygui.enterbox(title="W.I.L.L",msg="Please enter a command.")
-    if command_str:
-        answer = will.main(command_str)
-        easygui.msgbox(answer)
-        command()
-    else:
-        if easygui.ynbox("Would you like to exit?"):
-            sys.exit()
-        else:
-            command()
-command()
