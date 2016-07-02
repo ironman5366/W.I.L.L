8332e15b8e3129c872e6f25cb266e609df06b66e
Will <will.beddow@gmail.com>
2016-07-01 23:29:55 -0400

    Revert "Pip additions"
    This reverts commit 4e724db934516b391fa158a122938864c208af9f.

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from distutils.core import setup
-import os
-import sys
 setup(
   name = 'WillPy',
   packages = ['WillPy'],
-  version = '3.3',
+  version = '3.1',
   description = 'A smart personal assistant',
-install_requires=['beautifulsoup4', 'dateutils', 'easygui', 'expects', 'funcsigs', 'itsdangerous', 'MarkupSafe', 'mock', 'nose', 'pbr', 'PyDispatcher', 'python-dateutil', 'pytz', 'requests', 'six', 'termcolor', 'tinydb', 'voluptuous', 'websocket-client', 'Werkzeug', 'wikipedia', 'wolframalpha', 'spacy', 'keyring', 'pychromecast'],
   author = 'Will Beddow',
   author_email = 'WillPy@willbeddow.com',
   url = 'https://github.com/ironman5366/WillPy',
-  download_url = 'https://github.com/ironman5366/WillPy/tarball/3.2',
+  download_url = 'https://github.com/ironman5366/WillPy/tarball/3.1',
   keywords = ['Personal Assistant', 'Plugins'],
   classifiers = [],
 )
+def wolfram_setup():
+    wolframalpha_key = raw_input("Please enter a wolframalpha key. You can get one from http://products.wolframalpha.com/api/>")
+    if wolframalpha_key:
+        import WillPy.config as config
+        config.add_config({"wolfram" : [wolframalpha_key]})
+    else:
+        if raw_input("This is a required step for setup, are you sure you want to quit? (y/n)").lower() != "y":
+            wolfram_setup()
