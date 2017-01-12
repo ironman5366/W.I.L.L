1903a6112e0913cb636bee7c53c4a0639d9c1988
Will Beddow <will.beddow@gamil.com>
2017-01-12 16:25:44 -0500

    Deltee outdated db backup

--- core/dbbackup.py
+++
@@ -1,9 +0,0 @@
-#Quick utility for backing up and exporting userdata.
-#IMPORTANT: This offers no security and should not be used unless user data is being encrypted seperately
-import dataset
-
-db = dataset.connect('sqlite:///will.db')
-
-#Export data
-result = db['userdata'].all()
-dataset.freeze(result, format='json', filename='users.json')
