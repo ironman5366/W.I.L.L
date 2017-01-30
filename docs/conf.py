cfb1cf53f045ee2dde13015b2e028c6fe50593fc
Will Beddow <will.beddow@gamil.com>
2017-01-30 08:48:19 -0500

    :bug: Sphinx build error

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,6 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
      'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
